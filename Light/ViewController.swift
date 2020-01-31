70b03f9d3f4a047d8c7bbbface1328f9cd1d8c01
Raquel Santos <marinaluz371@gmail.com>
2020-01-30 17:48:18 -0800

    I finished adding the button to the phone

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
